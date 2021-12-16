8b991bdefeb7d44ee4921984a7792f71abbdba85
kimjy10 <kjy120924@gmail.com>
2021-12-16 17:22:35 +0900

    add DATABASES and SECRET_KEY on westagram/settings.py

--- students/kimjy/westagram/westagram/settings.py
+++ students/kimjy/westagram/westagram/settings.py
@@ -69,16 +69,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'westagram.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+SECRET_KEY=SECRET_KEY
+DATABASES=DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
