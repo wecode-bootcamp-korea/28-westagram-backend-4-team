55f2cfe9de3aedeaf478ae81f3a07b2c6134c243
nine57 <gusrn015@gmail.com>
2021-12-17 15:33:08 +0900

    Modify: User 테이블 최대 문자길이 수정

--- students/hyeongu/users/models.py
+++ students/hyeongu/users/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # models here.
 class User(models.Model):
-    full_name    = models.CharField(max_length = 10)                   # 이름
-    email        = models.CharField(max_length = 100)                  # 이메일
-    password     = models.CharField(max_length = 20, min_length = 8)   # 비밀번호 (8~20자)
-    phone_number = models.CharField(max_length = 20, null = True)      # 전화번호 (-를 포함한 숫자)
-    user_name    = models.CharField(max_length = 20)                   # 사용자 이름
-    birth_date   = models.DateField(null =  True)                      # 생년월일
+    full_name     = models.CharField(max_length = 50)                   # 이름
+    email         = models.CharField(max_length = 100)                  # 이메일
+    password      = models.CharField(max_length = 300)                  # 비밀번호 (8~20자)
+    mobile_number = models.CharField(max_length = 50, null = True)      # 전화번호 (-를 포함한 숫자)
+    username      = models.CharField(max_length = 50)                   # 사용자 이름
+    birth_date    = models.DateField(null =  True)                      # 생년월일
 
     class Meta:
         db_table = 'users'
