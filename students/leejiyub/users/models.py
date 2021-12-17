11e59f38d611159f5fabaf1d929b3eb223b73ce8
이지엽 <vvvv1541@naver.com>
2021-12-17 16:37:37 +0900

    project models setting correction

--- students/leejiyub/users/models.py
+++ students/leejiyub/users/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=256)
-    phonenumber = models.IntegerField()
-    gender = models.CharField(max_length=10)
-
+    name        = models.CharField(max_length=40)
+    email       = models.CharField(max_length=100)
+    password    = models.CharField(max_length=256)
+    phonenumber = models.CharField(max_length=50)
+    gender      = models.CharField(max_length=10)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+    
     class Meta:
         db_table = "users"    
     
