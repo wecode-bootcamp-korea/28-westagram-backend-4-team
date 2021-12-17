9b59396cf4caae23b895f502ef0b5973bc24e017
kimjy10 <kjy120924@gmail.com>
2021-12-17 13:25:46 +0900

    MODIFY: change instance argument in models.py

--- students/kimjy/westagram/users/models.py
+++ students/kimjy/westagram/users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     username      = models.CharField(max_length=45)
     password      = models.CharField(max_length=256)
-    email_address = models.CharField(max_length=300, null=True)
+    email_address = models.CharField(max_length=300, unique=True)
     phone_number  = models.CharField(max_length=50, null=True)
     profile_image = models.CharField(max_length=300, blank=True) 
     profile_bio   = models.CharField(max_length=500, blank=True)
