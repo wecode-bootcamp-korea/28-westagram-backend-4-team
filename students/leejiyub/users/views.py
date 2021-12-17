c2857961c8ea81fd500cc9f0344b86ecb8c70c0b
이지엽 <vvvv1541@naver.com>
2021-12-17 17:27:58 +0900

    project models setting correction1

--- students/leejiyub/users/views.py
+++ students/leejiyub/users/views.py
@@ -1,21 +1,3 @@
 from django.shortcuts import render
 
-
-import json
-
-from django.http     import JsonResponse
-from django.views    import View
-
-from users.models import User
-
-
-class UsersView(View):
-    def post(self, request):
-        data = json.loads(request.body)
-        user = User.objects.create(
-            name        =data['user_name'],
-            email       = data['user_email'],
-            password    = data['user_password'],
-            phonenumber = data['user_phonenumber'],
-            gender      = data['user_gender'],
-        )
+# Create your views here.
