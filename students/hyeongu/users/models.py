fabe398d8dd5a87c76795a596cf77773ea691512
nine57 <gusrn015@gmail.com>
2021-12-17 17:17:44 +0900

    Modify: model unique 설정 및 필드 추가, app setting 오설정 수정

--- students/hyeongu/users/models.py
+++ students/hyeongu/users/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # models here.
 class User(models.Model):
-    full_name     = models.CharField(max_length = 50)                   # 이름
-    email         = models.CharField(max_length = 100)                  # 이메일
-    password      = models.CharField(max_length = 300)                  # 비밀번호 (8~20자)
-    mobile_number = models.CharField(max_length = 50, null = True)      # 전화번호 (-를 포함한 숫자)
-    username      = models.CharField(max_length = 50)                   # 사용자 이름
-    birth_date    = models.DateField(null =  True)                      # 생년월일
+    full_name     = models.CharField(max_length = 50)
+    email         = models.CharField(max_length = 100, unique = True)
+    password      = models.CharField(max_length = 300)
+    mobile_number = models.CharField(max_length = 50, null = True, unique = True)
+    username      = models.CharField(max_length = 50, unique = True)
+    birth_date    = models.DateField(null = True)
+    created_at    = models.DateTimeField(auto_now_add = True)
+    updated_at    = models.DateTimeField(auto_now = True)
 
     class Meta:
         db_table = 'users'
-    
