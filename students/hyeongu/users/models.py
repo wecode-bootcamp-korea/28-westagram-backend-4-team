7a7986123f4608a4ab77a8a07244128851731d26
nine57 <gusrn015@gmail.com>
2021-12-17 17:26:26 +0900

    Modify : 오타 수정 및 table 속성 추가,변경 field unique 속성 추가 setting의 app 이름 오설정 변경 생성일자, 수정일자 model 추가

--- students/hyeongu/users/models.py
+++ students/hyeongu/users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# models here.
 class User(models.Model):
     full_name     = models.CharField(max_length = 50)
     email         = models.CharField(max_length = 100, unique = True)
--- students/hyeongu/users/tests.py
+++ students/hyeongu/users/tests.py
@@ -1,3 +1,1 @@
 from django.test import TestCase
-
-# tests here.
