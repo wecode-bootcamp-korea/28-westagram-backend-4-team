1b5e998910b3538f08ee2f865456a6d3bf9daf2b
kimjy10 <kjy120924@gmail.com>
2021-12-17 12:03:37 +0900

    modify: class name, instance name (mission 2)

--- students/kimjy/westagram/users/models.py
+++ students/kimjy/westagram/users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-# Create your models here.
-class Users(models.Model):
-    user_name     = models.CharField(max_length=45)
-    password      = models.CharField(max_length=50)
-    email         = models.CharField(max_length=300, null=True)
+
+class User(models.Model):
+    username      = models.CharField(max_length=45)
+    password      = models.CharField(max_length=256)
+    email_address = models.CharField(max_length=300, null=True)
     phone_number  = models.CharField(max_length=50, null=True)
     profile_image = models.CharField(max_length=300, blank=True) 
-    profile_desc  = models.CharField(max_length=500, blank=True)
+    profile_bio   = models.CharField(max_length=500, blank=True)
 
     class Meta:
         db_table = "users"
