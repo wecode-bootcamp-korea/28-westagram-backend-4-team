76f2e4115b9839b03d375f2aa492dc7f483e6750
이지엽 <vvvv1541@naver.com>
2021-12-17 17:29:34 +0900

    project models setting correction2

--- students/leejiyub/users/models.py
+++ students/leejiyub/users/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     name        = models.CharField(max_length=40)
     email       = models.CharField(max_length=100)
     password    = models.CharField(max_length=256)
-    phonenumber = models.CharField(max_length=50)
+    phonenumber = models.CharField(max_length=40)
     gender      = models.CharField(max_length=10)
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
