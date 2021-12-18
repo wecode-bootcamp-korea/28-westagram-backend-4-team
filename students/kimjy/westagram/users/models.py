855c33b65901b864537fa96fd199b21fb04c76e3
Jae Youp Kim <kjy120924@gmail.com>
2021-12-18 11:32:21 +0900

    change from blank to null

--- students/kimjy/westagram/users/models.py
+++ students/kimjy/westagram/users/models.py
@@ -6,8 +6,8 @@ class User(models.Model):
     password      = models.CharField(max_length=256)
     email_address = models.CharField(max_length=300, unique=True)
     phone_number  = models.CharField(max_length=50, null=True)
-    profile_image = models.CharField(max_length=1000, blank=True) 
-    profile_bio   = models.CharField(max_length=500, blank=True)
+    profile_image = models.CharField(max_length=1000, null=True) 
+    profile_bio   = models.CharField(max_length=500, null=True)
     created_at    = models.DateTimeField(auto_now_add=True)
     updated_at    = models.DateTimeField(auto_now=True)
 
