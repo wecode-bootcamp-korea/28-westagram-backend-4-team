9caf423c059b8e772ffc7a65b3f0aaa318bfd120
nine57 <gusrn015@gmail.com>
2021-12-18 15:52:51 +0900

    REMOVE : user table의 mobile_number unique 속성제거

--- students/hyeongu/users/models.py
+++ students/hyeongu/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     full_name     = models.CharField(max_length = 50)
     email         = models.CharField(max_length = 100, unique = True)
     password      = models.CharField(max_length = 300)
-    mobile_number = models.CharField(max_length = 50, null = True, unique = True)
+    mobile_number = models.CharField(max_length = 50, null = True)
     username      = models.CharField(max_length = 50, unique = True)
     birth_date    = models.DateField(null = True)
     created_at    = models.DateTimeField(auto_now_add = True)
